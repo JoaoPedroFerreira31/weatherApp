f8c38196affa411c4594163460d8305252dc85c9
JoaoPedroFerreira <jpmferreira@xnr-sisbi.com>
2023-08-18 16:09:01 +0000

    Updated navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -93,20 +93,21 @@
                 </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
-            <div class="flex items-center -mr-2 lg:hidden sm:block">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
-                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+        </div>
+        <!-- Hamburger -->
+        <div class="-mr-2 align-middle sm:my-auto sm:items-center md:hidden sm:flex sm:block lg:hidden">
+            <button @click="open = ! open" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500">
+                <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            </button>
         </div>
     </div>
 
+
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('dashboard') }}
@@ -121,6 +122,35 @@
             </div>
 
             <div class="mt-3 space-y-1">
+
+                <div class="relative inline-flex">
+                    <!-- Language -->
+                    <div class="" @click.away="menuToggle=false">
+                        <button class="py-2 pl-5 pr-3 text-gray-500 focus:outline-none" @click.prevent="menuToggle=!menuToggle">
+                            <span class="w-6 flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> <i class="mdi mdi-chevron-down"></i>
+                        </button>
+                        <div class="absolute right-0 w-48 mt-2 text-sm text-gray-700 origin-top-right bg-white rounded-md shadow-lg z-300" x-show="menuToggle" x-transition:enter="transition ease duration-300 transform" x-transition:enter-start="opacity-0 translate-y-2" x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease duration-300 transform" x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-4">
+                            <span class="absolute top-0 right-0 w-3 h-3 mr-3 -mt-1 transform rotate-45 bg-white"></span>
+                            <div class="relative w-full overflow-auto bg-white rounded-md ring-1 ring-black ring-opacity-5 z-300">
+                                <ul class="list-reset">
+                                    @foreach (Config::get('languages') as $lang => $language)
+                                        <li>
+                                            <a href="{{ route('lang.switch', $lang) }}" class="flex px-4 py-2 no-underline transition-colors duration-100 hover:bg-gray-100 hover:no-underline">
+                                                <span class="inline-block mr-2 flag-icon flag-icon-{{$language['flag-icon']}}"></span>
+                                                <span class="inline-block">{{$language['display']}}</span>
+                                                @if($lang === App::getLocale())
+                                                    <span class="ml-auto text-gray-700">
+                                                        <x-fas-check class="flex-shrink-0 w-4 h-4 mr-1"/>
+                                                    </span>
+                                                @endif
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
                 <x-responsive-nav-link :href="route('settings')">
                     <div class="inline-flex items-center gap-x-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
